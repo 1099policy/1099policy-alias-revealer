8aa4aa899c4ae32acc7b7754dc75727ca4cae58a
Cemre Fatih Karakulak <cradexco@gmail.com>
2022-05-11 02:55:19 +0300

    jsonify response

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
-    return request.json
+    return jsonify(request.json)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
